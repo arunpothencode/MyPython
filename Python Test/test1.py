d973654a41f29e412353da9d9def52114cd0a07f
Arun R <a4.arun@yahoo.com>
2021-11-25 01:26:05 +0530

    test code

--- Python Test/test1.py
+++ Python Test/test1.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import os
-import glob
 
-inppara = pd.read_csv(r"C:\Users\arun.r\Documents\Sharath\names.txt")
-inpfiles = glob.glob(r"C:\Users\arun.r\Documents\Sharath\f*.txt")
+inppara = pd.read_csv(r"C:\Users\arun.r\Downloads\OneDrive_1_11-18-2021\CRM_AU_11_18_2021.csv", delimiter = '|', encoding='latin-1')
 print(inppara)
-print(inpfiles)
