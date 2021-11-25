5e1af5f0ed8cbf49f3a60e7b4da60a12dac2aa98
Arun R <a4.arun@yahoo.com>
2021-11-26 00:41:15 +0530

    func added

--- Python Test/test1.py
+++ Python Test/test1.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import os
 
-inppara = pd.read_csv(r"C:\Users\arun.r\Downloads\OneDrive_1_11-18-2021\CRM_AU_11_18_2021.csv", delimiter = '|', encoding='latin-1')
-print(inppara)
+inppara = pd.read_csv(r"C:\Users\arun.r\Documents\Sharath\names.txt", delimiter=',', encoding='UTF-8', skiprows=[0])
+
+
+def myfunc(name):
+    print("Hello World " + name)
+
+
+for i in inppara:
+    myfunc(i)
